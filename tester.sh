f54327f4ab25b3616e2355f00a73ef7f05bd4d43
soufiane <soufianeessarhir@gmail.com>
2025-01-08 14:15:55 +0100

    Update tester.sh

--- tester.sh
+++ tester.sh
@@ -1,17 +1,95 @@
 #!/bin/bash
 
-count=0
-
-while [ $count -lt 1000 ] #you can change the numbe of tests by changing the value "1000" int the while "increace the value for mor tests an vice versa"
-do
-    test=$(seq 1 500 | sort -R) #you can change the size of the numbers list here by modefy the range (1 500) for 100 number use (1 100) or any range that have 100 numbers
-    result=$(./push_swap $test | wc -l)
-    if [ $result -gt 5500 ] #you can change the number of max instractions here by modefy the value (5500) for 500 number use 5500, for 100 use 700 ,5 use 12 ,3 use 2
-    then
-        echo "Result $result is greater than 5500, in case $test"
-		break #you can remove break to not stop at first fail
+# Function to test push_swap with given sequence
+test_push_swap() {
+    local sequence="$1"
+    local max_instructions="$2"
+    
+    result=$(./push_swap $sequence | wc -l)
+    
+    if [ $result -gt $max_instructions ]; then
+        echo "Failed: $result instructions (max: $max_instructions)"
+        echo "Sequence: $sequence"
+        return 1
+    else
+        echo "Pass: $result instructions"
+        return 0
     fi
-	   echo "$count    Result  $result  "
-    ((count++))
-done
-	echo "change the script to run other tests (3 5 100 500) follow the comments above"
+}
+
+# Generate permutations using factorial numbering system
+generate_unique_perm() {
+    local size="$1"
+    local nums=($(seq 1 $size))
+    local indices=($(seq 0 $((size-1))))
+    
+    # Fisher-Yates shuffle for initial randomization
+    for ((i = size - 1; i > 0; i--)); do
+        j=$((RANDOM % (i + 1)))
+        # Swap
+        temp=${nums[i]}
+        nums[i]=${nums[j]}
+        nums[j]=$temp
+    done
+    
+    echo "${nums[@]}"
+}
+
+# Main testing function
+run_tests() {
+    local size="$1"
+    local max_instructions="$2"
+    local num_tests="$3"
+    local count=0
+    local failed=0
+    
+    echo "Testing sequences of size $size (max $max_instructions instructions)"
+    echo "Running $num_tests tests..."
+    
+    while [ $count -lt $num_tests ]; do
+        sequence=$(generate_unique_perm $size)
+        
+        if ! test_push_swap "$sequence" "$max_instructions"; then
+            ((failed++))
+            echo "Failed test $((count+1))"
+            break  # Remove this line to continue after failures
+        fi
+        
+        ((count++))
+        echo "Completed test $count/$num_tests"
+    done
+    
+    if [ $failed -eq 0 ]; then
+        echo "All tests passed!"
+    else
+        echo "$failed tests failed"
+    fi
+}
+
+# Test configurations
+declare -A configs
+configs[3]=2    # 3 numbers: max 2 instructions
+configs[5]=12   # 5 numbers: max 12 instructions
+configs[100]=700  # 100 numbers: max 700 instructions
+configs[500]=5500 # 500 numbers: max 5500 instructions
+
+# Ask user which test to run
+echo "Select test size:"
+echo "1) 3 numbers"
+echo "2) 5 numbers"
+echo "3) 100 numbers"
+echo "4) 500 numbers"
+read -p "Enter choice (1-4): " choice
+
+case $choice in
+    1) size=3;;
+    2) size=5;;
+    3) size=100;;
+    4) size=500;;
+    *) echo "Invalid choice"; exit 1;;
+esac
+
+read -p "Number of tests to run (default 1000): " num_tests
+num_tests=${num_tests:-1000}
+
+run_tests $size ${configs[$size]} $num_tests
